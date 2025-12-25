4c086eec4771c657153e51452bb2c87e7db55eab
冰龍與雪風的工作室 <57035251+SetsuikiHyoryu@users.noreply.github.com>
2025-12-26 07:27:32 +0900

    chore(background): add simple background config

--- modules/background.lua
+++ modules/background.lua
@@ -1,13 +1,18 @@
 local module = {}
 
-function module.apply_to_config(config)
+local function use_simple_background(config)
+  config.window_background_opacity = 0.81
+end
+
+local function use_complex_background(config)
   config.background = {
     {
       source = {
         -- 必须是绝对路径。
         File = 'Users/hyoryu/dotfile/assets/neovim/n.jpeg',
+        -- File = 'Users/hyoryu/dotfile/assets/neovim/【哲风壁纸】孤独摇滚-粉色动漫少女.png',
         -- 直播用：
-        -- File = 'Users/hyoryu/dotfile/assets/neovim/d.png',
+        -- File = 'Users/hyoryu/dotfile/assets/neovim/131982759_p6.png',
       },
     },
 
@@ -19,6 +24,19 @@ function module.apply_to_config(config)
       width = '100%',
     },
   }
+end
+
+local function set_background(config, use_simple)
+  if use_simple then
+    use_simple_background(config)
+  else
+    use_complex_background(config)
+  end
+end
+
+function module.apply_to_config(config)
+  set_background(config, false)
+  config.text_background_opacity = 0.81 -- 影响 Tab 等图案，不影响文字。
 end
 
 return module
