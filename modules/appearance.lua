05248ba4ab19ae7d11cc7167840fb0df555a7b6d
冰龍與雪風的工作室 <57035251+SetsuikiHyoryu@users.noreply.github.com>
2025-12-26 07:25:42 +0900

    chore(appearance): disable titlebar

--- modules/appearance.lua
+++ modules/appearance.lua
@@ -1,5 +1,17 @@
 local module = {}
 
+-- 设置标题栏边框。
+--
+-- 无边框最大化工作流：
+--
+-- 1. `window_decorations` 设为默认后 `option + 绿色按钮最大化`。
+-- 2. `window_decorations` 设为 `RESIZE` 以取消边框，然后手动拉伸顶部。
+-- 3. `window_decorations` 设为 `NONE` 以阻止误触。
+local function set_window_decorations(config)
+  config.window_decorations = 'NONE'
+  config.adjust_window_size_when_changing_font_size = false
+end
+
 local function set_tab_bar(config)
   -- config.enable_tab_bar = false
   config.use_fancy_tab_bar = false
@@ -16,6 +28,7 @@ local function set_window_padding(config)
 end
 
 function module.apply_to_config(config)
+  set_window_decorations(config)
   set_tab_bar(config)
   set_window_padding(config)
 end
